9a1687ff3a2630354fc11d72092f43a743aa629b
Ebosos <zhelticov@gmail.com>
2022-08-21 16:51:36 +0700

    initial commit

---
+++ pick_a_card.rb
@@ -0,0 +1,5 @@
+
+values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+suist = ['Diamonds','Hearts','Clubs','Spades'] # Буби, Черви, Креси, Пики
+
+puts "#{values.sample} of #{suist.sample}"
